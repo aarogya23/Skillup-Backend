2df5ce880e6be992bd1af791937752972a117534
Aarogya <aarogyathapa375@gmail.com>
2026-01-17 16:41:09 +0545

    Update on the code implementation i.e. login things

---
+++ src/main/java/com/SkillUp/Controller/loginController.java
@@ -0,0 +1,42 @@
+package com.SkillUp.Controller;
+
+
+import com.SkillUp.Repository.UserRepository;
+import com.SkillUp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class loginController {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public boolean loginUser(@RequestBody User loginData) {
+
+        User user = null;
+
+        // Login using email + password
+        if (loginData.getEmail() != null) {
+            user = userRepository.findByEmailAndPassword(
+                    loginData.getEmail(),
+                    loginData.getPassword()
+            );
+        }
+
+        // Login using username + password
+        else if (loginData.getName() != null) {
+            user = userRepository.findByNameAndPassword(
+                    loginData.getName(),
+                    loginData.getPassword()
+            );
+        }
+
+        // Return true if user exists, false otherwise
+        return user != null;
+    }
+}
--- src/main/java/com/SkillUp/Repository/UserRepository.java
+++ src/main/java/com/SkillUp/Repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.SkillUp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     User findByEmailAndPassword(String email, String password);
 
     User findByNameAndPassword(String name, String password);
