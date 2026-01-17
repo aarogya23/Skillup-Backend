bdf017467f4be3d8cab8212658141463b0a59fa4
Aarogya <aarogyathapa375@gmail.com>
2026-01-17 16:37:29 +0545

    Update on the repository code for allowing both username and email both way to verify user with password

--- src/main/java/com/SkillUp/Repository/UserRepository.java
+++ src/main/java/com/SkillUp/Repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.SkillUp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    
+    User findByEmailAndPassword(String email, String password);
 
-
+    User findByNameAndPassword(String name, String password);
 }
