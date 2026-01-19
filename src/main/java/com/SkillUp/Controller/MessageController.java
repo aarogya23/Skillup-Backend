7962fc40fc65ed475390e16bc3dbdea25efd9ac1
Aarogya <aarogyathapa375@gmail.com>
2026-01-19 18:49:14 +0545

    Uopdate on the model i.e. dto to gather all the data

--- src/main/java/com/SkillUp/Controller/MessageController.java
+++ src/main/java/com/SkillUp/Controller/MessageController.java
@@ -1,4 +1,38 @@
 package com.SkillUp.Controller;
 
+import com.SkillUp.Websocket.ChatWebSocketHandler;
+import com.SkillUp.dto.MessageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/messages")
+@CrossOrigin("*")
 public class MessageController {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public MessageController(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
+    /**
+     * Send message via REST API and push via WebSocket
+     */
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(
+            @RequestBody MessageRequest request) throws IOException {
+
+        // Simple message format
+        String message =
+                request.getSender() + ": " + request.getContent();
+
+        // Broadcast to WebSocket clients
+        chatWebSocketHandler.broadcastMessage(message);
+
+        return ResponseEntity.ok("Message sent successfully");
+    }
 }
+
