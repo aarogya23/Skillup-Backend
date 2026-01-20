83260397e0f70bef1ae0eec3805e6a14adf17366
Aarogya <aarogyathapa375@gmail.com>
2026-01-20 15:39:33 +0545

    Update on the repository code

---
+++ src/main/java/com/SkillUp/Repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.SkillUp.Repository;
+
+
+import com.SkillUp.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Courses, Long> {
+    
+}
+
