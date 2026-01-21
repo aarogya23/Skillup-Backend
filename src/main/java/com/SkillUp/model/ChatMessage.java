7958cf3fc7d405e1f1467d92ade6f1534cc0e9c4
Aarogya <aarogyathapa375@gmail.com>
2026-01-21 15:55:31 +0545

    Adiing the model to save the data of user message

---
+++ src/main/java/com/SkillUp/model/ChatMessage.java
@@ -0,0 +1,58 @@
+package com.SkillUp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "chat_messages")
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sender;
+    private String content;
+    private String type; // CHAT, JOIN, LEAVE
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String sender, String content, String type) {
+        this.sender = sender;
+        this.content = content;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
