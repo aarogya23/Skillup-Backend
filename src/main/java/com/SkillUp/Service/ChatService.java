7c8dccf72ece13606bc21fa3b7e612616b5e841f
Aarogya <aarogyathapa375@gmail.com>
2026-01-21 15:59:08 +0545

    Update on the backend service for chat message

---
+++ src/main/java/com/SkillUp/Service/ChatService.java
@@ -0,0 +1,20 @@
+package com.SkillUp.Service;
+
+import com.SkillUp.Repository.ChatMessageRepository;
+import com.SkillUp.model.ChatMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final ChatMessageRepository repository;
+
+    public ChatService(ChatMessageRepository repository) {
+        this.repository = repository;
+    }
+
+    public ChatMessage saveMessage(ChatMessage message) {
+        return repository.save(message);
+    }
+
+}
