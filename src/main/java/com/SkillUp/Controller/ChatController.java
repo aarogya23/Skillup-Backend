316e74b317e9c007062af328c9b05e1c47f570b5
Aarogya <aarogyathapa375@gmail.com>
2026-01-21 16:00:52 +0545

    Update on the code implementation i.e. Implementation on the controller for sending and recciving the message

---
+++ src/main/java/com/SkillUp/Controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.SkillUp.Controller;
+
+import com.SkillUp.Service.ChatService;
+import com.SkillUp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage message) {
+        chatService.saveMessage(message);
+        return message;
+    }
+
+    @MessageMapping("/chat.join")
+    @SendTo("/topic/public")
+    public ChatMessage join(ChatMessage message) {
+        message.setContent(message.getSender() + " joined the chat");
+        message.setType("JOIN");
+        chatService.saveMessage(message);
+        return message;
+    }
+}
