d4390bcaa2a7bf1775a2c0d03cf099c46dcb0b08
Aarogya <aarogyathapa375@gmail.com>
2026-01-30 21:55:15 +0545

    Update on the code implementation

--- src/main/java/com/SkillUp/config/SecurityConfig.java
+++ src/main/java/com/SkillUp/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.SkillUp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
