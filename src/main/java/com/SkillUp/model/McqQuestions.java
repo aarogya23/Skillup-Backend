9c2c973f5d74fbbfc4e51ae45734abd39721f91c
Aarogya <aarogyathapa375@gmail.com>
2026-02-02 20:58:05 +0545

    Update on the model with the implementation of maven dependency

--- src/main/java/com/SkillUp/model/McqQuestions.java
+++ src/main/java/com/SkillUp/model/McqQuestions.java
@@ -1,4 +1,16 @@
 package com.SkillUp.model;
 
+
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class McqQuestions {
+
+    private Long id;
+    private String question;
+    private List<String> options;
+    private int correctOptionIndex; // 0-based index
 }
