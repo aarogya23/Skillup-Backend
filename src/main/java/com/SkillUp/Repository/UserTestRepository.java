45f3e899bdf682e5cb867676f75c63e801096198
Aarogya <aarogyathapa375@gmail.com>
2026-02-05 21:48:34 +0545

    Update the code implementation

---
+++ src/main/java/com/SkillUp/Repository/UserTestRepository.java
@@ -0,0 +1,11 @@
+package com.SkillUp.Repository;
+
+
+
+
+import com.SkillUp.model.UserTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTestRepository extends JpaRepository<UserTest, Long> {
+}
+
