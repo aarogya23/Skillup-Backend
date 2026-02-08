2995b28b92246dfcdb25b6af923a22ddb3b53a47
Aarogya <aarogyathapa375@gmail.com>
2026-02-08 22:03:48 +0545

    Update on the model code implementation

--- src/main/java/com/SkillUp/model/TestResult.java
+++ src/main/java/com/SkillUp/model/TestResult.java
@@ -1,20 +1,24 @@
 package com.SkillUp.model;
 
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
+@Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TestResult {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
     private int totalQuestions;
-    private int correctCount;
-    private int incorrectCount;
-
-    private List<QuestionResult> correctQuestions;
-    private List<QuestionResult> wrongQuestions;
+    private int correctAnswers;
+    private LocalDateTime date;
 }
-
